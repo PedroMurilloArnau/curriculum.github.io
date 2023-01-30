39b798618c9f196ed3eddd6f36f7ce859f32da6e
PedroMurilloArnau <Pedro.marnau@gmail.com>
2023-01-30 11:08:56 +0100

    first commit

---
+++ views/index.view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Curriculum</title>
+</head>
+<body>
+    <header>
+    <div>
+        <h1>Pedro Murillo</h1>
+    </div>
+    </header>
+    <div>
+        <p>Insert carousel</p>
+    </div>
+    <footer>
+        <div>
+            <P>A few information about me</p>
+        </div>
+    </footer>
+</body>
+</html>
